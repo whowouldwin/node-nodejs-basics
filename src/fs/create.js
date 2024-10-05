b3a9f77f8445a05a66b5965b080efdc0bd60416e
whowouldwin <leranetwork@gmail.com>
2024-10-05 20:12:07 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { access } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const filepath = path.join(__dirname, 'files', 'fresh.txt')
+    try {
+      await access(filepath)
+      throw new Error(`FS operation failed`);
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw error;
+      }
+    }
+    await fs.writeFile(filepath, 'I am fresh and young', )
+    console.log(`${filepath} created.`)
+  } catch (error) {
+    console.error(error.message)
+  }
 };
 
 await create();

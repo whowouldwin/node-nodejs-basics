281524cc72d6ccf026fd4707bce702cfabb54755
whowouldwin <leranetwork@gmail.com>
2024-10-07 17:18:42 +0300

    refactor: implement async pipeline for file hash calculation (calcHash.js)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,27 +1,29 @@
-import { createReadStream} from 'fs';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
 import path from 'path';
 import { createHash } from 'crypto'
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
   const filePath = path.join(__dirname, './files', 'fileToCalculateHashFor.txt');
   const hash = await createHash('sha256');
-  const readStream = createReadStream(filePath);
-
-  readStream.on('data', (chunk) => {
-    hash.update(chunk);
-  })
-
-  readStream.on('end', () => {
+  try {
+    await pipeline(
+      createReadStream(filePath),
+      async function* (source) {
+        for await (const chunk of source) {
+          hash.update(chunk);
+        }
+      }
+    );
     const fileHash = hash.digest('hex');
     console.log(`SHA256 hash is ${fileHash}`);
-  })
-
-  readStream.on('error', (err) => {
-    console.error(err);
-  })
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await calculateHash();

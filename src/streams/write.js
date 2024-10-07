cab19e08e3d30953ba3e40ff2a06875472b946c9
whowouldwin <leranetwork@gmail.com>
2024-10-07 18:30:13 +0300

    feat: implement write.js (src/streams)

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import { pipeline } from 'stream/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const filepath = path.join(__dirname, './files', 'fileToWrite.txt');
+    console.log('Enter the text you want to write to the file to the file');
+    console.log('Press Cmd+C (Linux) to end the input')
+    try {
+      await pipeline(
+        process.stdin,
+        createWriteStream(filepath)
+      );
+      console.log('Data has been written!');
+    } catch (e) {
+      console.log(e);
+    }
 };
 
 await write();

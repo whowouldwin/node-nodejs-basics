6dee8da12e30c0cb1a7bacc096d350ef0cc90194
whowouldwin <leranetwork@gmail.com>
2024-10-07 11:22:14 +0300

    refactor: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const env = process.env;
+    const rss = Object.keys(env)
+      .filter(key => key.startsWith('RSS_'))
+      .map(key => `${key}=${env[key]}`);
+
+    const result = rss.join('; ')
+    console.log(result);
 };
 
 parseEnv();

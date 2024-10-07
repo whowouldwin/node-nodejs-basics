defece5535b516fccfb0d111ab0000229847274e
whowouldwin <leranetwork@gmail.com>
2024-10-07 16:47:20 +0300

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createReadStream} from 'fs';
+import path from 'path';
+import { createHash } from 'crypto'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, './files', 'fileToCalculateHashFor.txt');
+  const hash = await createHash('sha256');
+  const readStream = createReadStream(filePath);
+
+  readStream.on('data', (chunk) => {
+    hash.update(chunk);
+  })
+
+  readStream.on('end', () => {
+    const fileHash = hash.digest('hex');
+    console.log(`SHA256 hash is ${fileHash}`);
+  })
+
+  readStream.on('error', (err) => {
+    console.error(err);
+  })
 };
 
 await calculateHash();
